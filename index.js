8575b5d3f2c8e6480faacc96142866e71c272319
stfrankies <63299700+stfrankies@users.noreply.github.com>
2022-04-11 22:57:31 +0300

    Ex.3.3: phonebook_backend, step4

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
 const express = require('express')
-
 const app = express()
 
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
-const currentDate = new Date();
+const currentdate = new Date();
+
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id);
+
+  if (!person) {
+    res.status(404).end()
+  }
+
+  res.json(person);
 })
 
 app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people</p> ${currentDate}`)
+  res.send(`<p>Phonebook has info for ${persons.length} people </p> ${currentdate}`)
 })
 
 const PORT = 3001
