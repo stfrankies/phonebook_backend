9e3c14fbf4d99e729b8c47f5e78c2459fd72a204
Frank Tunde <frankie@Frankie.local>
2022-05-19 12:59:08 +0300

    Update port variable

--- index.js
+++ index.js
@@ -97,6 +97,6 @@ app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people </p> ${currentdate}`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
